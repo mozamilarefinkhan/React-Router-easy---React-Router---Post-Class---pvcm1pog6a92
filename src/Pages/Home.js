cf0bb097fb198a9ea2dd5e9232f8af7375720963
Mozamil Arefin Khan <101820486+mozamilarefinkhan@users.noreply.github.com>
2022-08-23 02:38:54 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const Index = () => {
-  return <div id="index-page">Index Page</div>;
+export const Home = () => {
+  return <div id="home-page">Home Page</div>;
 };
